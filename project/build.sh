157f66124ae8bef06eea3eb5f5e624ad02340d7b
DreamEcho100 <maze6572198@gmail.com>
2025-11-26 12:38:40 +0200

    feat: init day1, it doesn't follow day 1, mostly a playground and some tests

--- project/build.sh
+++ project/build.sh
@@ -17,7 +17,7 @@ FLAGS="-Isrc -Wall -Wextra -std=c11"
 SRC="src/main.c src/game/game.c"
 
 if [ "$BACKEND" = "x11" ]; then
-    FLAGS="$FLAGS -DUSE_X11 -lX11"
+    FLAGS="$FLAGS -DUSE_X11 -lX11 -lXext -lXft $(pkg-config --cflags --libs freetype2)"
     SRC="$SRC src/platform/x11_backend.c"
 elif [ "$BACKEND" = "raylib" ]; then
     FLAGS="$FLAGS -DUSE_RAYLIB -lraylib -lm -ldl -lpthread"
